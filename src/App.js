edd624fe56d5368d2e846504af5301e2187d6a21
Jacob Singer <jacobbradleysinger@gmail.com>
2023-03-02 22:53:27 -0600

    confirm serial connection, install environment on linux computer

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import { Box, Button, Text } from '@chakra-ui/react';
 
 function App() {
   const handleButtonPress = async () => {
-    const port = await navigator.serial.requestPort();
-    await port.open({ baudRate: 9600 });
-    const writer = port.writable.getWriter();
-    // Send out data we don't care what it is
-    const data = "motor 0 moved";
-    writer.write(data).then(() => {
-      // Data written successfully
+    try {
+      console.log(navigator.serial);
+      const port = await navigator.serial.requestPort();
+      await port.open({ baudRate: 9600 });
+      const writer = port.writable.getWriter();
+      // Send out data we don't care what it is
+      const encoder = new TextEncoder();
+      const data = "motor 0 moved";
+      const arrayBuffer = encoder.encode(data);
+      await writer.write(arrayBuffer);
+      await writer.abort();
       console.log(JSON.stringify(data));
-    }).catch(error => {
-      // Error writing data
-    });
+      port.close();
+    } catch (error) {
+      console.log(error);
+    }
 
   };
 
