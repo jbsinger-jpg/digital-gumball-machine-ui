780cb8fdab21d5104b91e261f0f265243b0f39f5
JACOB SINGER <jacobbradleysinger@gmail.com>
2023-03-02 20:53:16 -0600

    rough draft

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { Box, Button, HStack, Text } from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box h={"100vh"} w={"100vw"} bgColor="#15142A">
+      <Box display={"flex"} position="absolute" top="20px" justifyContent={"center"} alignItems="center" w="100%">
+        <Box>
+          <Text color="white">
+            Header
+          </Text>
+        </Box>
+      </Box>
+      <Box alignItems={"center"} justifyContent="center" width="100%" height={"100%"} display="flex">
+        <Text color={"white"}>
+          Body
+        </Text>
+      </Box>
+      <Box display={"flex"} position="absolute" bottom="20px" justifyContent={"center"} alignItems="center" w="100%">
+        <Box>
+          <HStack gap="2" width={"100%"}>
+            <Button>Send Serial </Button>
+            <Text color="white">
+              Footer
+            </Text>
+          </HStack>
+        </Box>
+      </Box>
+    </Box >
   );
 }
 
