2153bf464fd21a2220a0a5749042d47d77297b3f
Singer <p326n848@wichita.edu>
2023-04-06 17:23:39 -0500

    add game logic in with animated component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,18 @@
 import { Box, Button } from '@chakra-ui/react';
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import MyContext from '../Context';
+import { motion } from 'framer-motion';
 
 export default function HomePage() {
     const navigate = useNavigate();
     const { myContextState: port } = useContext(MyContext);
+    const [playGame, setPlayGame] = useState(false);
+
+    const MotionBox = motion(Box);
 
     const handleButtonPress = async () => {
+        setPlayGame(true);
         const writer = port.writable.getWriter();
         const encoder = new TextEncoder();
         const data = "motor 0 moved";
@@ -18,8 +23,21 @@ export default function HomePage() {
     };
     return (
         <Box height="100vh" width="100vw" alignItems="center" display="flex" justifyContent={"center"} backgroundColor="#080808">
+            {playGame &&
+                <MotionBox
+                    initial={{ scale: 0 }}
+                    animate={{ scale: 1 }}
+                    transition={{
+                        type: "spring",
+                        stiffness: 260,
+                        damping: 20
+                    }}
+                >
+                    <iframe src="https://www.friv.com/z/games/tetrablocks/game.html" width="600" height="450" frameborder="0"></iframe>
+                </MotionBox>
+            }
             <Box position="absolute" bottom={10} height="10vh" width="100vw" paddingLeft={"10px"} justifyContent="space-between" paddingRight={"10px"} display="flex">
-                <Button onClick={handleButtonPress}>Get Candy</Button>
+                <Button onClick={handleButtonPress}>Get Candy/Play Game</Button>
                 <Button onClick={() => {
                     navigate("/");
                     port.close();
